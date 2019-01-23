bc9c6ecd387d703d49117a370172c85e9b9fe0be
Stoyan Hristov <shristov@intracol.com>
2019-01-24 00:10:37 +0200

    make the dynamic client run sucessfully with the sample client

--- gRPC-client/src/test/java/org/tues/stefchog/polyglot/TestClient.java
+++ gRPC-client/src/test/java/org/tues/stefchog/polyglot/TestClient.java
@@ -6,6 +6,10 @@
 package org.tues.stefchog.polyglot;
 
 import com.google.common.collect.ImmutableList;
+import com.google.protobuf.DescriptorProtos;
+import com.google.protobuf.Descriptors;
+import com.google.protobuf.DynamicMessage;
+import com.sdt.gtl.polyglot.ProtobufEnvelope;
 import org.tues.stefchog.polyglot.ConfigProto;
 import org.tues.stefchog.polyglot.ConfigProto.OutputConfiguration;
 import org.tues.stefchog.polyglot.ConfigProto.OutputConfiguration.Destination;
@@ -18,6 +22,8 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -34,9 +40,10 @@ public static void main(String[] args) {
                 setDestination(Destination.STDOUT).build();
         Output output = Output.forConfiguration(outputconf);
         
+        String protoRoot = "src//test//java//org//tues//stefchog//polyglot//proto";
         ConfigProto.ProtoConfiguration protoConfig
                 = ConfigProto.ProtoConfiguration.newBuilder().
-                        setProtoDiscoveryRoot("../proto/").
+                        setProtoDiscoveryRoot(protoRoot).
                         setUseReflection(true).build();
         List<Path> list = new ArrayList();
         ImmutableList<Path> unmodifiableList = ImmutableList.copyOf(list);
@@ -44,15 +51,33 @@ public static void main(String[] args) {
         ConfigProto.CallConfiguration callConfig =  
                 ConfigProto.CallConfiguration.newBuilder().
                         setUseTls(false).build();
- 
+       
+        ImmutableList<DynamicMessage> requestMessageList = constructRequestMessage();
         ServiceCall.callEndpoint(output,
                 protoConfig,
-                Optional.of("localhost:4556"),
+                Optional.of("localhost:6565"),
                 Optional.of("blueprint.AccountService/register"),
-                Optional.of(Paths.get("../proto/")),
+                Optional.of(Paths.get(protoRoot)),
                 Optional.empty(),
                 unmodifiableList,
-                callConfig);
+                callConfig,
+                requestMessageList);
+    }
+    
+     private ImmutableList<DynamicMessage> constructRequestMessage() {
+        ProtobufEnvelope pe = new ProtobufEnvelope();
+        pe.<String>addField("email", "test@email.com", DescriptorProtos.FieldDescriptorProto.Type.TYPE_STRING);
+        pe.<String>addField("password", "abc", DescriptorProtos.FieldDescriptorProto.Type.TYPE_STRING);
+        DynamicMessage m = null;
+        try {
+            m = pe.constructMessage("RegisterRequest");
+        } catch (Descriptors.DescriptorValidationException ex) {
+            Logger.getLogger(TestClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        List<DynamicMessage> messageList = new ArrayList();
+        messageList.add(m);
+        ImmutableList<DynamicMessage> requestMessageList = ImmutableList.copyOf(messageList);
+        return requestMessageList;
     }
 
 }
